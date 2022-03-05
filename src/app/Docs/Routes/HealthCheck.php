5574f7b7ee260f1538bc0fce5f321434f1722999
webber.wu <webber.wu@tenoz.tw>
2022-03-06 01:17:28 +0800

    [develop] add swagger

---
+++ src/app/Docs/Routes/HealthCheck.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ */
---
+++ src/app/Docs/Server.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @OA\server(
+ *      url = "http://localhost:8000",
+ *      description="本地端"
+ * )
+ */
